c8206667fb8e2f589ec527ec80d921cdb7091199
Alex Donnelly <a.james.donnelly@gmail.com>
2020-07-19 21:55:07 -0500

    initial setup commit for css, js and html

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+
+// outlook for multiple cities
+
+//dashboard with form inputs to search for a city
+//presented with current and future conditions for that city and that city is added to the search history
+//view current weather conditions for that city
+// presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//I view the UV index
+//presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//view future weather conditions for that city
+// presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// local storage-you can click on a city in the search history
+//again presented with current and future conditions for that city
